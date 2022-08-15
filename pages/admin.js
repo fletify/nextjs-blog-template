4a2c8483748397064d96513e0eee2126bd616a76
SiSH0001 <snehasishray.acs9c@gmail.com>
2022-08-15 23:38:56 +0530

    m

--- pages/admin.js
+++ pages/admin.js
@@ -4,9 +4,6 @@ export default function Home() {
   return (
     <>
       <Head>
-        <meta charset="UTF-8"/>
-        <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
         <title>Admin | Blog - Fletify</title>
         <meta name="keywords" content="admin, fletify, blog"/>
         <meta name="description" content=""/>
